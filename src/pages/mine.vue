4c2bfa7e688eda3ca98cef5d69721a3a20a8ba7d
Spanky(Mac) <819062398@qq.com>
2019-03-15 16:39:45 +0800

    first commit

---
+++ src/pages/mine.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="akk" @click="doSth">
+    213123
+  </view>
+</template>
+<script>
+  export default {
+    data() {
+      return {};
+    },
+    methods: {
+      doSth: function() {
+        uni.navigateTo({
+          url: '/pages/PersonalCenter'
+        });
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .akk {
+    color: red;
+  }
+</style>
